84c2a377fa67705bdbafa0dcb0ee2b86230cfec2
giesler <giesler@LAPRP003.cmd-intern.de>
2018-03-16 11:17:14 +0100

    LeihverträgeBean fertig bearbeitet

--- src/main/java/car4me/ejb/LeihvertragBean.java
+++ src/main/java/car4me/ejb/LeihvertragBean.java
@@ -5,7 +5,11 @@
  */
 package car4me.ejb;
 
+import car4me.jpa.Kunde;
 import car4me.jpa.Leihvertrag;
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -23,4 +27,29 @@ public class LeihvertragBean extends EntityBean<Leihvertrag, Long>{
     
     @PersistenceContext
     EntityManager em;
+    
+    public Leihvertrag createLeihvertrag(Leihvertrag leihvertrag) throws Exception{
+        Long help = leihvertrag.getFahrzeug().getId();
+        Date beginnDatum = leihvertrag.getBeginnDatum();
+        Date endeDatum = leihvertrag.getEndeDatum();
+        List <Leihvertrag> help2 = em.createQuery("SELECT l FROM Leihvertrag l WHERE l.getFahrzeug().getId() = :help AND l.getEndeDatum <= :beginnDatum AND l.getBeginnDatum >= :endeDatum")
+                                     .setParameter("help", help)
+                                     .setParameter("beginnDatum", beginnDatum)
+                                     .setParameter("endeDatum", endeDatum)
+                                     .getResultList();
+        
+        if(help2 == null){
+            return this.saveNew(leihvertrag);
+        }else{
+            throw new Exception("Hilfe");
+        }
+        
+        
+    }
+    
+    public List<Leihvertrag> getLeihvertrage(Kunde k){
+        return em.createQuery("SELECT l FROM Leihvertrag l WHERE l.getKunde().getId() = :k.getId()")
+                .setParameter("k", k)
+                .getResultList();
+    }
 }
